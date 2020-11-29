d0da6257b9b3ebe185f036f3d4f201cb3290d4fb
Chuckles <chuckleberryfinn@gmail.com>
2020-11-29 16:52:34 +0000

    Add Results to main

--- src/bin/q1-1.rs
+++ src/bin/q1-1.rs
@@ -1,5 +1,8 @@
-fn main() {
+use aoc::*;
+
+fn main() -> Result<()> {
     println!("Day 1 part 1");
+    Ok(())
 }
 
 #[cfg(test)]
--- src/bin/q1-2.rs
+++ src/bin/q1-2.rs
@@ -1,5 +1,8 @@
-fn main() {
+use aoc::*;
+
+fn main() -> Result<()> {
     println!("Day 1 part 2");
+    Ok(())
 }
 
 #[cfg(test)]
