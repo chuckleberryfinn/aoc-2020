ab0e9bf07e7b24eb54cf7aaf0ca74ad22ac361aa
Chuckles <chuckleberryfinn@gmail.com>
2020-12-01 13:08:27 +0000

    Add day 1 part 2

--- src/bin/q1-2.rs
+++ src/bin/q1-2.rs
@@ -1,14 +1,39 @@
 use aoc::*;
 
 fn main() -> Result<()> {
-    println!("Day 1 part 2");
+    let result = run();
+    println!("Day 1 part 2 result is {}", result);
     Ok(())
+}
+
+fn run() -> i64 {
+    let expenses: Vec<i64> = input("1.txt")
+        .unwrap()
+        .lines()
+        .map(|e| e.parse().unwrap())
+        .collect();
+    let mut solution = -1;
+
+    for i in 0..expenses.len() {
+        for j in (i + 1)..expenses.len() {
+            for k in (j + 1)..expenses.len() {
+                let (x, y, z) = (expenses[i], expenses[j], expenses[k]);
+                if (x + y + z) == 2020 {
+                    solution = x * y * z;
+                    break;
+                }
+            }
+        }
+    }
+    solution
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn test() {
-        assert!(true);
+    fn test_run() {
+        assert!(run() == 201251610);
     }
 }
